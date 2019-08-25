db89d6ed4b2a0a63a7c6cbca122e79cc048a4285
YURIc5 <arewane.c5@gmail.com>
2019-08-25 19:47:00 +0900

    ルーティングとテキストフィールドの準備

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,24 @@
+$(function() {
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーは見つかりません");
+      }
+    })
+    
+  });
+});
