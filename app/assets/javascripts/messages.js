272a4d7473a69b025f81f12e3dc4658903dc5cb1
YURIc5 <arewane.c5@gmail.com>
2019-08-25 11:57:28 +0900

    HTMLを追加した分メッセージ画面を下にスクロール

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,16 +1,26 @@
-$(function(){
+$(document).on('turbolinks:load',function(){
   function buildMessage(message){
-    var html = `<p class="message__upper-info__talker">
-                  ${message.user_name}
-                </p>
-                <p class="message__upper-info__date">
-                ${message.date}
-                </p>
-                <p class="message__text">
-                  ${message.content}
-                </p>`
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                    ${message.user_name}
+                    </p>
+                    <p class="message__upper-info__date">
+                    ${message.date}
+                    </p>
+                  </div>
+                  <div class="lower-message">
+                    <p class="message__text">
+                    ${content}
+                    </p>
+                  </div>
+                  ${img}
+                </div>`
     return html;
   }
+  
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -27,9 +37,12 @@ $(function(){
       var html = buildMessage(message);
       $('.messages').append(html);
       $('#message_content').val('')
+      $('.submit-btn').prop('disabled', false);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })
     .fail(function(){
       alert('error');
+      $('.submit-btn').prop('disabled', false);
     })
   })
 });
