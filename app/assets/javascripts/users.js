e26030a1c43b15d54256ae704bd9e5ed56cb668b
YURIc5 <arewane.c5@gmail.com>
2019-08-26 12:29:00 +0900

    非同期通信の結果を得て、HTMLを作成とサインアップ時のエラー修正

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,12 @@
 $(function() {
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="6" data-user-name=${user.name}>追加</a>
+                </div>`
+    $('#user-search-result').append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     $.ajax({
@@ -19,6 +27,8 @@ $(function() {
         appendErrMsgToHTML("一致するユーザーは見つかりません");
       }
     })
-    
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    })
   });
 });
